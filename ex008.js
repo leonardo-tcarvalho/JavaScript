c6f6e1c8df5e26c202c1bbf280c40cd4bae289bd
leonardo-carvalho <leonardotadeu.carvalho@gmail.com>
2023-12-12 11:44:03 -0300

    Revert "Create ex008.js"
    This reverts commit 049f39e5928ce8d72546a8322552276dc25b878e.

--- ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 67
-console.log(`A velocidade do seu carro é de ${vel}Km/h`)
-console.log('Dirija sempre usando cinto de segurança')
-if (vel > 60.5){
-    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
-}
