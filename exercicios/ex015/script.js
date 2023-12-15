a49b2b3e8d944efcd597d3f3fce575361710bd03
leonardo-carvalho <leonardotadeu.carvalho@gmail.com>
2023-12-15 14:54:43 -0300

    Update
    Trocando cores de utilizando o JS.

--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -14,24 +14,32 @@ function verificar(){
     if (sexo[0].checked) {
         if (idade >= 0 && idade < 12) {
             genero = 'Menino'
+            document.body.style.backgroundColor = '#b8fdff'
         } else if (idade < 18) {
             genero = 'Adolescente'
+            document.body.style.backgroundColor = '#86f9fd'
         } else if (idade <= 60) {
             genero = 'Homem'
+            document.body.style.backgroundColor = '#4ff9ff'
         } else if (idade > 60) {
             genero = 'Senhor'
+            document.body.style.backgroundColor = '#00f7ff'
         }
         pronome = 'um'
         res.innerHTML = `A pessoa identificada é ${pronome} ${genero} de ${idade} anos.`
     } else if (sexo[1].checked) {
         if (idade >= 0 && idade < 12) {
             genero = 'Menina'
+            document.body.style.backgroundColor = '#ffc0cb'
         } else if (idade < 18) {
             genero = 'Adolescente'
+            document.body.style.backgroundColor = '##ff758c'
         } else if (idade <= 60) {
             genero = 'Mulher'
+            document.body.style.backgroundColor = '#ff4060'
         } else if (idade > 60) {
             genero = 'Senhora'
+            document.body.style.backgroundColor = '#ff002b'
         }
         pronome = 'uma'
         res.innerHTML = `A pessoa identificada é ${pronome} ${genero} de ${idade} anos.`
